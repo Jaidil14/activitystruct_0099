d81a74f023067c60181a88c7cd370947736fe9c7
Jaidil14 <jaidilrachmad@gmail.com>
2024-04-01 16:46:55 +0700

    Menambahkan end line untuk merapikan tampilan

--- MateriMingguKe6.cpp
+++ MateriMingguKe6.cpp
@@ -31,15 +31,18 @@ int main() {
         cout << "Umur Mahasiswa : ";
         cin >> mhs[i].umur;
         cin.ignore(1,'\n');
+        cout << endl;
     }
 
     for (int i = 0; i < 3; i++) {
         cout << endl;
+        cout << "Data Mahasiswa ke-" << (i + 1) << ":" << endl;
         cout << "\nNIM : " << mhs[i].nim;
         cout << "\nNama : " << mhs[i].nama;
         cout << "\nAlamat : ";
         cout << "\n \t Desa : " << mhs[i].alamat.desa;
         cout << "\n \t Kota : " << mhs[i].alamat.kota;
         cout << "\nUmur : " << mhs[i].umur;
+        cout << endl;
     }
 }
